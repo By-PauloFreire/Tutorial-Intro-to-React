2c2fae23245a4eab48dca5bd749a4a4f28694dfd
Paulo Freire <paulgarden95@gmail.com>
2022-04-19 16:20:46 -0300

    feat: component Board

--- src/index.js
+++ src/index.js
@@ -3,12 +3,42 @@ import { ReactDOM } from "react";
 import './index.css';
 
 class Square extends React.Component {
-    render() {
-      return (
-        <button className="square">
-          {/* TODO */}
-        </button>
-      );
-    }
+  render() {
+    return (
+      <button className="square">
+        {/* TODO */}
+      </button>
+    );
   }
-  
+}
+
+class Board extends React.Component {
+  renderSquare(i) {
+    return <Square />;
+  }
+
+  render() {
+    const status = 'Next player: X';
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
