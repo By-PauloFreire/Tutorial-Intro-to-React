7697f2a938e4e7c5821591bba0227bac1c84f91c
Paulo Freire <paulgarden95@gmail.com>
2022-04-19 16:12:56 -0300

    feat: component Square

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { ReactDOM } from "react";
+import './index.css';
+
+class Square extends React.Component {
+    render() {
+      return (
+        <button className="square">
+          {/* TODO */}
+        </button>
+      );
+    }
+  }
+  
