b1aaa21172ae5c62ba9fec8ca663ba2eb6a906a5
Paulo Freire <paulgarden95@gmail.com>
2022-04-19 23:06:45 -0300

    feat: Game component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { ReactDOM } from "react";
+import ReactDOM from "react-dom";
 import './index.css';
 
 class Square extends React.Component {
@@ -42,3 +42,24 @@ class Board extends React.Component {
     );
   }
 }
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
